33b9247619fcdf62f5eb8229ba7ee1d5946dceb9
Breno Henrique <brenohenrique137@outlook.com>
2026-03-13 20:15:11 -0300

    Filmes e Séries

---
+++ src/movieserie/Flick.java
@@ -0,0 +1,47 @@
+package movieserie;
+
+import java.util.Scanner;
+
+public class Flick {
+	public static void main (String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String nome = "";
+		while(nome.isEmpty()) {
+			System.out.print("Nome: ");
+			nome = entrada.nextLine().trim().toUpperCase();
+		}
+		
+		String filmeSerie = "";
+		while(filmeSerie.isEmpty()) {
+			System.out.print("Filme ou Série: ");
+			filmeSerie = entrada.nextLine().trim().toUpperCase();
+		}
+		
+		String genero = "";
+		while (genero.isEmpty()) {
+			System.out.print("Gênero: ");
+			genero = entrada.nextLine().trim().toUpperCase();
+		}
+		
+		String notaStr = "";
+		float notaF = -1;
+		while(notaF < 0 || notaF > 10) {
+			System.out.print("Nota: ");
+			notaStr = entrada.nextLine().trim().replace(",", ".");
+			
+			try {
+				notaF = Float.parseFloat(notaStr);
+			} catch (NumberFormatException e) {
+				System.out.print("\n\nErro: Insira um número de 0 a 10!");
+				
+				notaF = -1;
+			}
+		}
+		
+		System.err.printf("\n\n%s - %s - %s - %.1f", nome, filmeSerie, genero, notaF);
+		
+		entrada.close();
+	}
+}
